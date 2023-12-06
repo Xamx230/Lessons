e0165b78c132faa61aa11f30bca5dbbec3798c32
Xamx230 <149161615+Xamx230@users.noreply.github.com>
2023-12-06 21:26:21 +0300

    Add files via upload

---
+++ ot_20_do_90/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] fgh = { 10, 11, 12, 25, 30, 45, 64, 78, 98, 87 };
+int count = 0;
+int index = 0;
+
+while (index < fgh.Length)
+{
+    if (fgh[index] >= 20 && fgh[index] <= 90)
+    {
+        count++;
+    }
+    index++;
+}
+Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке от 20 до 90: {count}");
---
+++ чётные_числа/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+int[] a = {11,12,13,14,51,611,17,91,120};
+int count = 0;
+foreach(int num in a){
+if(num % 2 == 0){
+    count++;
+}
+}
+
+Console.WriteLine($"коль-во чётных чисел в массиве {count}");
