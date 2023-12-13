4374a546d126eb1255274819ff9998f5aa1fd716
Xamx230 <149161615+Xamx230@users.noreply.github.com>
2023-12-13 21:36:48 +0300

    Add files via upload

---
+++ swap_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿void SwapArray<T>(List<T> list){
+    if (list == null || list.Count <= 1)
+    {
+        return;
+    }
+
+    T value = list[0];
+    list.RemoveAt(0);
+    SwapArray(list);
+    list.Add(value);
+}
+List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+SwapArray(list);
+Console.WriteLine(string.Join(',', list));
