8ef91eda2f86d9ef6fe1c14de530c4617de17b8a
Xamx230 <149161615+Xamx230@users.noreply.github.com>
2023-12-13 21:20:11 +0300

    Add files via upload

---
+++ Akkerman/Program.cs
@@ -0,0 +1,10 @@
+﻿int AkkermanNumbers(int m, int n){
+if (m == 0) return n + 1;
+else if (n == 0) return AkkermanNumbers(m - 1, 1);
+else return AkkermanNumbers(m - 1, AkkermanNumbers(m, n - 1));
+}
+
+int m = 2;
+int n = 3;
+
+Console.Write($"Функция Аккермана равна {AkkermanNumbers(m, n)} ");
